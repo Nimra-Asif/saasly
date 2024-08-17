8a1d9dc92cc4b7bd3454dcb13aa84b1af238494c
Headbreakerr <133957591+Headbreakar@users.noreply.github.com>
2024-08-18 01:59:57 +0500

    made the controller and set the basic homepage route
    CSS, JS and assets don't work.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\publicController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Corrected route
+Route::get('/', [publicController::class, 'index'])->name('homepage');

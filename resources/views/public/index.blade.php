bf6eb352c44af8c88cfb4754d9fff8fe20c1bee3
Nimra-Asif <138553410+Nimra-Asif@users.noreply.github.com>
2024-08-18 02:54:37 +0500

    Update index.blade.php

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="Sassly HTML5 Template">
 
-  <title>Sassly Online Meeting</title>
+  <title>Sui Dhaga</title>
 
   <!-- Fav Icon -->
   <link rel="icon" type="image/x-icon" href="https://crowdytheme.com/html/assets/imgs/logo/favicon.webp">
